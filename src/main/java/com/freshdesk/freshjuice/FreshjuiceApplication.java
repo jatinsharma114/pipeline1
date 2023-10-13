f63deb38deac28da169820095fce51a6c0347203
jatinsharma114 <jatin2010sharma@gmail.com>
2023-10-13 20:16:43 +0530

    juice

---
+++ src/main/java/com/freshdesk/freshjuice/FreshjuiceApplication.java
@@ -0,0 +1,13 @@
+package com.freshdesk.freshjuice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshjuiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshjuiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freshdesk/freshjuice/FreshjuiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshdesk.freshjuice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshjuiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
