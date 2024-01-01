c3879be542c9c9540dbb1b61bcea89367ff143ba
jatinsharma114 <jatin2010sharma@gmail.com>
2024-01-01 16:32:21 +0530

    adding the endpoint

---
+++ src/main/java/com/freshdesk/freshjuice/Home.java
@@ -0,0 +1,15 @@
+package com.freshdesk.freshjuice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @GetMapping
+    public String home() {
+        return "This application is running fine!!";
+    }
+
+
+}
